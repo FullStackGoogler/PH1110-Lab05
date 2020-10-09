2974fd21e1270593bacf4cc6f7d681bb1b7f4d28
Eric Zhou <71947872+FullStackGoogler@users.noreply.github.com>
2020-10-09 14:31:50 -0400

    Update DEV.py

--- DEV.py
+++ DEV.py
@@ -1,26 +1,26 @@
 #Lab #5 Python Code, ezzhou
 
 #Mass of Cart (kg)
-mCart1 = 500
-mCart2 = 499.94
+mCart1 = #massOfCart1
+mCart2 = #massOfCart2
 mDEV = 0.0001 #assuming cart was measured with a triple beam balance
 
 #Collision Type
-collision = "Elastic"
+collision = #collisionType
 
 #Initial Velocity of Cart1 (m/s)
-Vi1 = 0
-Vi1DEV = 0.002
+Vi1 = #ViOfCart1
+Vi1DEV = #uncertaintyOfVi1
 #Initial Velocity of Cart2 (m/s)
-Vi2 = 0.358
-Vi2DEV = 0.003
+Vi2 = #ViOfCart2
+Vi2DEV = #uncertaintyOfVi2
 
 #Final Velocity of Cart1 (m/s)
-Vf1 = 0.282
-Vf1DEV = 0.020
+Vf1 = #VfOfCart1
+Vf1DEV = #uncertaintyOfVf1
 #Final Velocity of Cart2 (m/s)
-Vf2 = -0.024
-Vf2DEV = 0.008
+Vf2 = #VfOfCart2
+Vf2DEV = #uncertaintyOfVf2
 
 #Initial Momentum of Cart 1 (kgm/s)
 Pi1 = round(Vi1 * mCart1, 3)
@@ -36,7 +36,7 @@ Pf1DEV = round(Pf1 * (Vf1DEV/Vf1 + mDEV/mCart1), 3)
 Pf2 = round(Vf2 * mCart2, 3)
 Pf2DEV = round(Pf2 * (Vf2DEV/Vf2 + mDEV/mCart2), 3)
 
-#Initial Kinetic Energy of Cart 1 (J) --------------------------> Double Check if KEDEV should be V^2 or just V
+#Initial Kinetic Energy of Cart 1 (J)
 KEi1 = round(0.5 * mCart1 * (Vi1 ** 2), 3)
 KEi1DEV = round(KEi1 * (mDEV/mCart1), 3)
 #Initial Kinetic Energy of Cart 2 (J)
@@ -49,8 +49,6 @@ KEf1DEV = round(KEf1 * (mDEV/mCart1 + Vf1DEV/Vf1), 3)
 #Final Kinetic Energy of Cart 2 (J)
 KEf2 = round(0.5 * mCart2 * (Vf2 ** 2), 3)
 KEf2DEV = round(KEf2 * (mDEV/mCart2 + Vf2DEV/Vf2), 3)
-
-#
 
 #Print Results
 print(collision, "collision data:")
